48d5a48de49e51e4beb9a60f4ec9bcab48228258
vladimir.senchenko <vladimir.senchenko@academmedia.com>
2016-06-29 12:38:35 +0600

    New time scale script

---
+++ Assets/Scripts/ScaleTimeInSpace.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleTimeInSpace : MonoBehaviour {
+	public Rigidbody rigidbody;
+	private bool customTime;
+	private float gravityScale;
+	// Use this for initialization
+	void Awake () {
+		rigidbody = gameObject.GetComponent<Rigidbody> ();
+	}
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.T)) {
+			rigidbody.velocity = -Vector3.forward * 30;
+		}
+	}
+
+	void FixedUpdate () {
+		if (customTime) {
+			rigidbody.AddForce (Physics.gravity * gravityScale);// * Time.fixedTime;
+		}
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Time") {
+			rigidbody.velocity /= 10f;
+			rigidbody.angularVelocity /= 10f;
+			rigidbody.useGravity = false;
+			gravityScale = 0.1f;
+			customTime = true;
+		}
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		if (col.gameObject.tag == "Time") {
+			rigidbody.velocity *= 10f;
+			rigidbody.angularVelocity *= 10f;
+			rigidbody.useGravity = true;
+			gravityScale = 0.1f;
+			customTime = false;
+		}
+	}
+}
---
+++ Assets/VideoCntrlr.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VideoCntrlr : MonoBehaviour {
+	
+	// Use this for initialization
+	void Awake () {
+		Renderer r = GetComponent<Renderer>();
+		MovieTexture movie = (MovieTexture)r.material.mainTexture;
+		movie.loop = true;
+		movie.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
