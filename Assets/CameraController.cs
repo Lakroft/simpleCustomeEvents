71e98480649e836ce7b138f26a977e677496d07e
Lakroft <lakroft36@gmail.com>
2016-08-27 23:28:14 +0700

    added camera follow script for 3-rd pes.

---
+++ Assets/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	public Transform targetPos;
+	public Transform playerPos;
+	private Transform transform;
+	private float speed = 0;
+	private float filterValue = 0.03f;
+	private Vector3 moveToVector;
+	//private float oldSpeed;
+	// Use this for initialization
+	void Start () {
+		if(targetPos == null) this.enabled = false;
+		transform = gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Vector3.Distance(transform.position, targetPos.position) > 0.1f)
+		{
+//			speed = lineFilter (speed, Vector3.Distance(transform.position, targetPos.position));
+//			transform.position += (targetPos.position - transform.position).normalized * speed;
+			transform.position = lineFilter(transform.position, targetPos.position, Vector3.Distance(transform.position, targetPos.position));
+		}
+		transform.LookAt(playerPos.position);
+	}
+
+	float lineFilter (float oldValue, float newValue)
+	{
+		return oldValue * (1 - filterValue) + newValue * filterValue;
+	}
+
+	Vector3 lineFilter (Vector3 oldValue, Vector3 newValue, float inersia)
+	{
+		return oldValue * (1 - filterValue * inersia) + newValue * filterValue * inersia;
+	}
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 1;
+	public float angularSpeed = 1;
+	private CharacterController controller;
+	private Transform transform;
+	private Vector3 rotateVector = Vector3.zero;
+	// Use this for initialization
+	void Start () {
+		controller = gameObject.GetComponent<CharacterController>();
+		transform = gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		controller.Move(transform.forward * Input.GetAxis("Vertical") * Time.deltaTime * speed);
+		//transform.Rotate(transform.rotation.eulerAngles + rotateVector);
+		transform.RotateAround(transform.position, transform.up, Input.GetAxis("Horizontal") * angularSpeed);
+		//Input.GetAxis("Horizontal");
+	}
+}
